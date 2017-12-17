6d9029e43f998361c4a112228f627479b837e653
Michael Rose <elementation@gmail.com>
2017-12-17 11:29:21 -0700

    Day17 solution

---
+++ src/main/java/aoc2017/Day17.java
@@ -0,0 +1,50 @@
+package aoc2017;
+
+import aoc.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day17 {
+    public static void main(String[] args) {
+        Util.assertThat(spinlockP1(2017, 3)).isEqualTo(638);
+
+        System.out.println("Part 1: " + spinlockP1(2017, 386));
+        System.out.println("Part 2: " + spinlockP2(50_000_000, 386));
+    }
+
+    private static int spinlockP1(int iterations, int steps) {
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+
+        int pos = 0;
+        for (int i = 0; i < iterations; i++) {
+            for (int j = 0; j < steps; j++) {
+                pos = (pos+1)%list.size();
+            }
+            pos++;
+            list.add(pos, i+1);
+        }
+
+        return list.get((pos+1)%list.size());
+    }
+
+    private static int spinlockP2(int iterations, int steps) {
+        int zeroPos = 0;
+        int valueAfterZero = 0;
+        int pos = 0;
+        for (int i = 1; i <= iterations; i++) {
+            pos = (pos+steps)%(i) + 1;
+
+            if (pos == zeroPos+1) {
+                valueAfterZero = i;
+            }
+
+            if (pos < zeroPos) {
+                zeroPos++;
+            }
+        }
+
+        return valueAfterZero;
+    }
+}
