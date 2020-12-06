39bb128e3a9ef00849c11c486ea64fdaab5fe503
Michael Rose <elementation@gmail.com>
2020-12-06 11:43:06 -0700

    util: add reduce implementation

--- util/src/aoc/mod.rs
+++ util/src/aoc/mod.rs
@@ -147,3 +147,25 @@ impl<T> SliceExt for [T] {
         }
     }
 }
+
+// Reduce implementation by dtolnay, until fold_first() is stablized.
+pub trait Reduce<T> {
+    fn reduce<F>(self, f: F) -> Option<T>
+        where
+            Self: Sized,
+            F: FnMut(T, T) -> T;
+}
+
+impl<T, I> Reduce<T> for I
+    where
+        I: Iterator<Item = T>,
+{
+    #[inline]
+    fn reduce<F>(mut self, f: F) -> Option<T>
+        where
+            Self: Sized,
+            F: FnMut(T, T) -> T,
+    {
+        self.next().map(|first| self.fold(first, f))
+    }
+}
