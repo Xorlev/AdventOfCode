94ecd928e5d53582c9d380f356982d6ee56eb7bf
Michael Rose <elementation@gmail.com>
2020-12-07 21:46:39 -0700

    day 7: memoize graph search in part 1

--- aoc2020/src/bin/main_07.rs
+++ aoc2020/src/bin/main_07.rs
@@ -42,22 +42,18 @@ struct Bag {
 }
 
 fn part1(bag_graph: &HashMap<String, Vec<Bag>>) -> usize {
-    fn search(bag_graph: &HashMap<String, Vec<Bag>>, key: &str) -> bool {
-        let mut search_queue = VecDeque::new();
-        let mut visited_bags = HashSet::new();
-        search_queue.push_back(key);
-        visited_bags.insert(key);
+    fn search<'a: 'b, 'b>(bag_graph: &'a HashMap<String, Vec<Bag>>, has_gold_bag: &'b mut HashMap<&'a str, bool>, bag: &str) -> bool {
+        if let Some(contains_bag) = has_gold_bag.get(bag) {
+            return *contains_bag;
+        }
 
-        while let Some(bag) = search_queue.pop_front() {
-            if let Some(held_bags) = bag_graph.get(bag) {
-                for bag in held_bags {
-                    if bag.bag_type == "shiny gold" {
-                        return true;
-                    }
-
-                    if visited_bags.insert(&bag.bag_type) {
-                        search_queue.push_back(&bag.bag_type);
-                    }
+        if let Some(held_bags) = bag_graph.get(bag) {
+            for bag in held_bags {
+                if bag.bag_type == "shiny gold" || search(&bag_graph, has_gold_bag, &bag.bag_type) {
+                    has_gold_bag.insert(&bag.bag_type, true);
+                    return true;
+                } else {
+                    has_gold_bag.insert(&bag.bag_type, false);
                 }
             }
         }
@@ -65,9 +61,10 @@ fn part1(bag_graph: &HashMap<String, Vec<Bag>>) -> usize {
         false
     }
 
+    let mut has_gold_bag: HashMap<&str, bool> = HashMap::new();
     bag_graph
         .keys()
-        .filter(|&key| search(bag_graph, key))
+        .filter(|&key| search(bag_graph, &mut has_gold_bag, key))
         .count()
 }
 
