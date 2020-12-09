84f83595775e5d43bb86ef23f2b191360a3631e4
Michael Rose <elementation@gmail.com>
2020-12-08 23:38:22 -0700

    day 9

---
+++ aoc2020/src/bin/main_09.rs
@@ -0,0 +1,70 @@
+use failure::bail;
+use itertools::{Itertools, MinMaxResult};
+use util::aoc::*;
+
+fn main() -> AocResult<()> {
+    let outputs: Vec<i64> = input::read(9)?
+        .iter()
+        .map(|line| line.parse::<i64>().map_err(|e| e.into()))
+        .collect::<AocResult<Vec<_>>>()?;
+
+    result("Part 1", || part1(&outputs))?;
+    result("Part 2", || part2(&outputs))?;
+
+    Ok(())
+}
+
+fn part1(outputs: &[i64]) -> AocResult<i64> {
+    find_weak_number(outputs, 25)
+}
+
+fn part2(outputs: &[i64]) -> AocResult<i64> {
+    let weak_number = find_weak_number(outputs, 25)?;
+
+    for window_size in 2..outputs.len() / 2 {
+        let mut i = 1;
+        let mut sum: i64 = outputs[0..window_size].iter().sum();
+        while i + window_size < outputs.len() {
+            let end = i + window_size - 1;
+
+            sum -= outputs[i - 1];
+            sum += outputs[end];
+
+            if sum == weak_number {
+                let contiguous_set = &outputs[i..end];
+                if let MinMaxResult::MinMax(min, max) = contiguous_set.iter().cloned().minmax() {
+                    return Ok(min + max);
+                } else {
+                    panic!("output set had less than 2 values: {:?}", contiguous_set);
+                }
+            }
+
+            i += 1;
+        }
+    }
+
+    bail!("failed to find contiguous set")
+}
+
+fn find_weak_number(outputs: &[i64], preamble_size: usize) -> AocResult<i64> {
+    for i in preamble_size..outputs.len() {
+        if let None = find_pair(&outputs[i - preamble_size..i], outputs[i]) {
+            return Ok(outputs[i]);
+        }
+    }
+
+    bail!("didn't find weak number")
+}
+
+fn find_pair(outputs: &[i64], target: i64) -> Option<(i64, i64)> {
+    for i in 0..outputs.len() {
+        for output_j in &outputs[i..] {
+            let output_i = outputs[i];
+            if output_i != *output_j && output_i + output_j == target {
+                return Some((output_i, *output_j));
+            }
+        }
+    }
+
+    None
+}
