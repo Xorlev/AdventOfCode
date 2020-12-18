b6e4b7013761b50eab5bd7bd775452bb2eee3d23
Michael Rose <elementation@gmail.com>
2020-12-18 00:06:43 -0700

    rustfmt

--- aoc2020/src/bin/main_15.rs
+++ aoc2020/src/bin/main_15.rs
@@ -7,7 +7,8 @@ use std::str::FromStr;
 use util::aoc::*;
 
 fn main() -> AocResult<()> {
-    let input = input::read(15)?[0].split(",")
+    let input = input::read(15)?[0]
+        .split(",")
         .map(|s| s.parse::<i64>().map_err(|e| e.into()))
         .collect::<AocResult<Vec<i64>>>()?;
 
@@ -30,7 +31,7 @@ fn solve(starting_numbers: &[i64], end_turn: usize) -> i64 {
     let mut turn = 1;
     let mut last_spoken = -1;
     while turn <= end_turn {
-        let mut to_speak = starting_numbers.get(turn-1).copied().unwrap_or(0);
+        let mut to_speak = starting_numbers.get(turn - 1).copied().unwrap_or(0);
         if let Some(previously_spoken_turn) = number_at_turn.insert(last_spoken, turn - 1) {
             to_speak = turn as i64 - 1 - previously_spoken_turn as i64;
         }
